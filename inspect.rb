5c6ea9c9102636ddc7344e53476c58bb2a202713
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-07-07 13:54:21 +0900

    refactoring

--- inspect.rb
+++ inspect.rb
@@ -4,17 +4,15 @@ results = []
 total = 0
 
 Dir.glob('*.log').each do |file|
-  total = 0
   details = []
 
   File.open(file).each do |row|
     profiles = row.split(' ')
-    total += profiles[0].to_i
     details << {mem: profiles[0].to_i, loc: profiles[1]}
   end
 
   results << { time: file.split('.').first,
-                total: total,
+                total: details.map{|detail| detail[:mem]}.inject(:+),
                 top3: details.sort_by{|detail| detail[:mem]}.reverse[0..2]
               }
 end
