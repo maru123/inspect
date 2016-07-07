bc0b2e913b6fbc356c580c0552d886b405f2a0cd
Itsumi Maruyama <maruyama.itsumi@b-basics.co.jp>
2016-07-07 14:03:33 +0900

    refactoring

--- inspect.rb
+++ inspect.rb
@@ -1,18 +1,15 @@
 require 'pp'
 
-results = []
-total = 0
-
-Dir.glob('*.log').each do |file_name|
+results = Dir.glob('*.log').map do |file_name|
   details = File.foreach(file_name).map do |row|
     profiles = row.split(' ')
     {mem: profiles.first.to_i, loc: profiles.last}
   end
 
-  results << { time: file_name.split('.').first,
-                total: details.map{|detail| detail[:mem]}.inject(:+),
-                top3: details.sort_by{|detail| detail[:mem]}.reverse[0..2]
-              }
+  { time: file_name.split('.').first,
+    total: details.map{|detail| detail[:mem]}.inject(:+),
+    top3: details.sort_by{|detail| detail[:mem]}.reverse[0..2]
+  }
 end
 
 pp results
